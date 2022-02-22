e701f1d366160a3b5dff9c73945556b44df66194
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-22 14:58:41 +0100

    Add style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,14 +3,30 @@
     <div class="container">
       <div class="row">
         <div class="col d-flex justify-content-between align-items-center">
-          <img src="../assets/img/MasterStudy-1.svg" alt="" />
+          <img src="../assets/img/MasterStudy-1.svg" alt="Logo" />
           <div id="navbar">
             <ul class="d-flex">
               <li><a href="#">Courses</a></li>
               <li><a href="#">Course Formats</a></li>
-              <li><a href="#">Add Course</a></li>
+              <li>
+                <a href="#">Add Courses<span>NEW</span></a>
+              </li>
               <li><a href="#">Pages</a></li>
               <li><a href="#">Demos</a></li>
+              <div class="d-flex ps-5">
+                <li>
+                  <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                </li>
+              </div>
             </ul>
           </div>
         </div>
@@ -20,7 +36,9 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Header",
+};
 </script>
 
 <style lang="scss" scoped>
@@ -33,13 +51,23 @@ header {
 
     a {
       text-decoration: none;
-      padding-left: 20px;
+      padding-left: 15px;
       color: black;
 
       .fa-brands {
         color: black;
       }
     }
+  }
+  span {
+    background-color: red;
+    font-size: 8px;
+    padding: 2px 5px;
+    border-radius: 2px;
+    position: relative;
+    bottom: 15px;
+    right: 15px;
+    color: white;
   }
 }
 </style>
