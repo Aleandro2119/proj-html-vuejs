0898d5a0e7126d2104a392dc5939cbbe46028131
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-22 13:43:56 +0100

    Add students

---
+++ src/components/Students.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <h2 class="text-center">What Students Say</h2>
+        <div class="col-3 d-flex align-items-center justify-content-center">
+          <i class="fa-solid fa-arrow-left"></i>
+        </div>
+        <div class="col-6">
+          <div class="d-flex justify-content-center align-items-center mt-5">
+            <img
+              class="rounded-circle"
+              src="../assets/img/1-100x100.jpg"
+              alt="1"
+            />
+            <img
+              class="rounded-circle p-3"
+              src="../assets/img/2-100x100.jpg"
+              alt="2"
+            />
+            <img
+              class="rounded-circle"
+              src="../assets/img/4-100x100.jpg"
+              alt="3"
+            />
+          </div>
+          <div class="text-center">
+            <h4>Paints of the Future</h4>
+            <i v-for="n in 5" :key="n" class="fa-solid fa-star"></i>
+            <p>
+              The response to your MasterStudy has been really overwhelming!
+              Those who participated in the workshop are spreading the word here
+              on campus and the "buzz" is on. The VP of Instruction wants you to
+              come back! Her goal is to have more faculty tiained. She also
+              wants to attend a workshop herself. Our President told me
+              Masterstudy needs to be the cornerstone of our sucess program.
+            </p>
+          </div>
+        </div>
+        <div class="col-3 d-flex align-items-center justify-content-center">
+          <i class="fa-solid fa-arrow-right"></i>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "students",
+};
+</script>
+
+<style>
+</style>
