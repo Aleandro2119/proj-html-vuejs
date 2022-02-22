229689adb39d73c1642582f5e6ba621be4544643
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-22 10:53:07 +0100

    Add maintop

---
+++ src/components/MainTop.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="main-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <h4 class="pb-3">
+            Hey there, my name is <br />
+            John Doe. I'm the founder <br />
+            of the Brain Academy.
+          </h4>
+          <img class="img-fluid" src="../assets/img/sign-184x108.jpg" alt="" />
+        </div>
+        <div class="col-7">
+          <p>
+            A wonderful serenity has taken possession of my entire soul, like
+            these sweet <br />
+            mornings of spring which I enjoy with my whole heart. I am alone,
+            and feel the charm <br />
+            of existence in this spot, which was created for the bliss of souls
+            like mine. I am so <br />
+            happy, my dear friend, so absorbed in the exquisite sense of mere
+            tranquil existence, <br />
+            that I neglect my talents. When, while the lovely valley teems with
+            vapour around me, <br />
+            and the meridian sun strikes the upper surface of the impenetrable
+            follage of my <br />
+            trees, and but a few stray gleams steal into the inner sanctuary.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.main-top {
+  background-color: white;
+  margin-top: 120px;
+}
+</style>
