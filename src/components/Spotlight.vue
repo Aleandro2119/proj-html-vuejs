6e8c6ffcb8cc36b034a8941e07ca662bad011101
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-22 17:10:49 +0100

    Add style

--- src/components/Spotlight.vue
+++ src/components/Spotlight.vue
@@ -2,9 +2,26 @@
   <section class="spotlight mt-5">
     <div class="container">
       <div class="row">
-        <div class="col-12">
-          <div class="col-3">
-            <i class="bi bi-people p-4"></i>
+        <div class="col-12 d-flex flex-wrap position">
+          <div class="col-3 d-flex justify-content-center align-items-center">
+            <i class="fa-solid fa-user-group"> </i>
+            <span>3195</span>
+            <span class="text-white">Online Learner</span>
+          </div>
+          <div class="col-3 d-flex justify-content-center align-items-center">
+            <i class="fa-solid fa-film"> </i>
+            <span>23</span>
+            <span class="text-white">Video Courses</span>
+          </div>
+          <div class="col-3 d-flex justify-content-center align-items-center">
+            <i class="fa-solid fa-image"> </i>
+            <span>437</span>
+            <span class="text-white">Sliders & Images</span>
+          </div>
+          <div class="col-3 d-flex justify-content-center align-items-center">
+            <i class="fa-solid fa-thumbs-up"> </i>
+            <span>1440</span>
+            <span class="text-white">Excellent Reviews</span>
           </div>
         </div>
       </div>
@@ -15,33 +32,6 @@
 <script>
 export default {
   name: "Spotlight",
-
-  data() {
-    return {
-      boxes: [
-        {
-          number: "3195",
-          text: "Online Learner",
-          url: "#",
-        },
-        {
-          number: "23",
-          text: "Video Courses",
-          url: "#",
-        },
-        {
-          number: "437",
-          text: "Sliders & Images",
-          url: "#",
-        },
-        {
-          number: "1440",
-          text: "Excellent Reviews",
-          url: "#",
-        },
-      ],
-    };
-  },
 };
 </script>
 
@@ -52,5 +42,27 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
   height: 450px;
+}
+
+.fa-solid {
+  font-size: 30px;
+  color: white;
+}
+
+.position {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+span {
+  color: #f2b91e;
+  font-size: 20px;
+  padding-left: 10px;
+}
+
+p {
+  color: white;
+  padding-left: 20px;
 }
 </style>
