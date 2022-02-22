f1867c7316c1c451202dc40f2b08df48cb1da94d
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-22 10:52:33 +0100

    Import pages

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
     <Header />
+    <Jumbotron />
+    <MainTop />
+    <CardsCourses />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Jumbotron from "./components/Jumbotron.vue";
+import MainTop from "./components/MainTop.vue";
+import CardsCourses from "./components/CardsCourses.vue";
+
 export default {
   name: "App",
   components: {
     Header,
+    Jumbotron,
+    MainTop,
+    CardsCourses,
   },
 };
 </script>
 
 <style lang="scss">
 @import "./assets/scss/style.scss";
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
 </style>
