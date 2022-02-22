f4d05201a72ecff50c059263018467f387991f62
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-22 16:43:47 +0100

    Add style

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,15 +1,15 @@
 <template>
   <section>
     <div class="container">
-      <h2 class="text-center p-4">Why my Courses?</h2>
-      <div class="row gy-5">
+      <h2 class="text-center pt-5 pb-5">Why my Courses?</h2>
+      <div class="row">
         <div class="col-12 d-flex flex-wrap">
           <div
             v-for="(element, index) in elements"
             :key="index"
-            class="col-6 p-4 box"
+            class="col-6 p-4 box mb-4"
           >
-            <h4 class="">{{ element.title }}</h4>
+            <h4>{{ element.title }}</h4>
             <p>{{ element.text }}</p>
           </div>
         </div>
@@ -27,18 +27,22 @@ export default {
       elements: [
         {
           title: "Entrepreneurship",
+          icon: "fa-solid fa-suitcase",
           text: "Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate",
         },
         {
           title: "Accelerated learning",
+          icon: "fa-solid fa-rocket",
           text: "Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate",
         },
         {
           title: "Productivity",
+          icon: "fa-solid fa-thumbs-up",
           text: "Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate",
         },
         {
           title: "Life Coaching",
+          icon: "fa-solid fa-gear",
           text: "Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate",
         },
       ],
@@ -51,5 +55,9 @@ export default {
 .box {
   background-color: white;
   border-left: 4px solid #f2b91e;
+
+  p {
+    color: silver;
+  }
 }
 </style>
